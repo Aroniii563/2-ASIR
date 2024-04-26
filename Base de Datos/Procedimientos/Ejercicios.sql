d175700584482bbc34df98b99d7d57d0b14360fd
Aroniii <140091318+Aroniii563@users.noreply.github.com>
2024-04-26 13:25:29 +0100

    Update Ejercicios.sql

--- Base de Datos/Procedimientos/Ejercicios.sql
+++ Base de Datos/Procedimientos/Ejercicios.sql
@@ -55,3 +55,49 @@ end
 delimiter ;
 
 call vis_nombre_cliente2(4, @micliente);
+
+/* Crea un procedimiento almacenado que tome dos parámetros de entrada: 
+cliente_id y antiguedad_en_meses. Verifica si la antigüedad del cliente 
+es mayor a 12 meses, y si lo es, establece un nuevo aumento del 20% en su 
+limite de crédito; de lo contrario, se establece un estandar estándar del 
+10%. Luego, actualiza el descuento del cliente en la tabla Clientes según 
+el cliente_id proporcionado. */
+
+DELIMITER //
+
+
+CREATE PROCEDURE actualizarLimiteCredito(
+
+    IN cliente_id INT,
+
+    IN antiguedad_en_meses INT
+
+)
+
+BEGIN
+
+    DECLARE nuevo_limite DECIMAL(15,2);
+  
+    -- Verificar si la antigüedad es mayor a 12 meses
+
+    IF antiguedad_en_meses > 12 THEN
+
+        SET nuevo_limite = 0.20; -- Nuevo aumento
+
+    ELSE
+
+        SET nuevo_limite = 0.10; -- Nuevo aumento
+
+    END IF;
+
+    -- Actualizar el descuento del cliente
+    start transaction;
+    UPDATE Clientes
+
+    SET limitecredito = limitecredito+limitecredito*nuevo_limite
+
+    WHERE codigocliente = cliente_id;
+
+END //
+
+DELIMITER ;
