56045dcff7b81848665650aa9559459b1d89adb1
Aroniii <140091318+Aroniii563@users.noreply.github.com>
2024-05-22 12:27:13 +0100

    Update Ejercicios.sql

--- Base de Datos/Insert Into/Ejercicios.sql
+++ Base de Datos/Insert Into/Ejercicios.sql
@@ -4,7 +4,8 @@ delete from notas where notas.dni =
 (select alumnos.dni from alumnos where APENOM = 'Alcalde García, Elena')
 and cod = (select cod from asignaturas where nombre = 'Análisis');
 
-/* Insertar un nuevo alumno, de nombre Juan Sin Miedo y con DNI:45566778, y el resto de los datos, con lo  que quieras. */
+/* Insertar un nuevo alumno, de nombre Juan Sin Miedo y con DNI:45566778, y 
+el resto de los datos, con lo  que quieras. */
 
 insert into alumnos values
 ('45566778', 'Sin Miedo, Juan', 'C/León', 'Fuenteovejuna', '922584687');
